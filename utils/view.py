d79df882a5f3cec48fd0d7879f36a16443b64ecb
Nataliamroz123 <163434752+Nataliamroz123@users.noreply.github.com>
2024-06-03 16:51:53 +0200

    feat: read functionalities

---
+++ utils/view.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import requests
+import tkintermapview
+from bs4 import BeautifulSoup
+inmport psycopg2 as ps
+
+#db_params = ps.connect(
+#   database = "carrental"
+#    user = "postgres"
+#    password = "<PASSWORD>"
+#    host = "localhost"
+#    port = ""
+#)
